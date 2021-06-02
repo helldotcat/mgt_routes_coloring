84caea98c3b7ba3ade88f26ae7e35ccb8495d16a
Grigory <gs.ponomarenko@yandex.ru>
2021-06-03 00:10:43 +0300

    Add metro coloring

---
+++ metro_graph.js
@@ -0,0 +1,29 @@
+// create a network
+var container = document.getElementById("mynetwork");
+var data = {
+  nodes: nodes,
+  edges: edges,
+};
+var options = {
+  nodes: {
+    shape: "circle",
+  },
+  edges: {
+  	color: {
+  		inherit: "both"
+  	},
+  	selectionWidth: 5,
+  },
+  layout: {
+    randomSeed: 191006,    // Optional; insert your value here
+    improvedLayout: true
+  },
+
+  physics: {
+    enabled: true,
+    // stabilization: {
+    //   iterations: 20     // Change this to whatever is convenient for you
+    // }
+  },
+};
+var network = new vis.Network(container, data, options);
